23b945428df8b0138a20e94bf8e6598ad6c445bd
Zhora <Ideana.it@yahoo.comIdeana.it@yahoo.comIdeana.it@yahoo.com>
2025-04-27 21:12:25 +0200

    Project Initialized

---
+++ TheTask/TheTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheTask
+//
+//  Created by Zhora Babakhanyan on 27/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheTask/TheTask/TheTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTaskApp.swift
+//  TheTask
+//
+//  Created by Zhora Babakhanyan on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TheTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
